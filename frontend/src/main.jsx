79525aabca8d2238eda875d069ca2de14f39a584
s758282x <duong.anthony0@utexas.edu>
2025-04-27 00:57:11 -0500

    Update main.jsx
    update config to deployed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,8 +10,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider
       config={{
-        signInRedirectURL: "http://localhost:5173/",
-        signOutRedirectURL: "http://localhost:5173/",
+        signInRedirectURL: "https://nice-field-0795aca10.6.azurestaticapps.net/callback",
+        signOutRedirectURL: "https://nice-field-0795aca10.6.azurestaticapps.net/",
         clientID: "6qduOWvfDIUlDQWHOWOb9w27fl4a",
         baseUrl: "https://api.asgardeo.io/t/utmis372",
         scope: ["openid", "profile", "email"],
