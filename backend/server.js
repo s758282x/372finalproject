14282a0044cca802dad704c0ba92cb566a5cbb52
s758282x <duong.anthony0@utexas.edu>
2025-04-26 21:39:18 -0500

    Update server.js
    changed the hardcoded port

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const spinsRouter = require("./routes/spins");
-const authRouter = require("./routes/auth"); 
+const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
-const betsRouter = require("./routes/bets"); 
-const aiRouter = require("./routes/ai"); 
+const betsRouter = require("./routes/bets");
+const aiRouter = require("./routes/ai");
 const blogsRouter = require("./routes/blogs");
 
 const app = express();
@@ -13,19 +13,17 @@ app.use(express.json());
 
 // Routes
 app.use("/api/spins", spinsRouter);
-app.use("/api/auth", authRouter); 
+app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/bets", betsRouter);
-app.use("/api/ai", aiRouter); 
+app.use("/api/ai", aiRouter);
 app.use("/api/blogs", blogsRouter);
-
-
 
 // Test route
 app.get("/", (req, res) => res.send("Roulette backend is live!"));
 
 // Start server
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
